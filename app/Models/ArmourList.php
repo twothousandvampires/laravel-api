4a7f2c5ed43e6453beb8cf473cee85697c35b7b4
nikita <twothousandvampires@gmail.com>
2022-03-29 17:49:13 +0300

    nwm

---
+++ app/Models/ArmourList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ArmourList extends Model
+{
+    use HasFactory;
+
+    protected $table = 'armours_list';
+}
